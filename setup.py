01aac51f9ed2502f7397b1828a7643e93288ce96
David Matthews <David.Matthews.1@uvm.edu>
2019-07-05 18:20:12 -0400

    Adding support for this to be an installable package

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='EvoSoroCore',
+      author='David Matthews',
+      version='0.1',
+      packages=['evosorocore'],
+      description='Evolution toolkit for soft robots',
+      install_requires=['numpy', 'scipy', 'networkx', 'lxml']
+      )
