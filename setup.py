72cf9e8e42af5efde7bf4f676721365bd1cce297
David Matthews <David.Matthews.1@uvm.edu>
2019-07-10 15:29:49 -0400

    modifying version number to be consistant with my other projects

--- setup.py
+++ setup.py
@@ -1,8 +1,17 @@
 from setuptools import setup
 
+
+
+# Version Numbers:
+# x.y.z.w
+# w: Minor changes; Does not impact API.
+# z: Minor changes: Backwards compatible.
+# y: Semi-minor changes: Not all parts backwards compatible, you probably will not need to change your code.
+# x: Major changes: Upgrades will likely require changes to your code.
+
 setup(name='EvoSoroCore',
       author='David Matthews',
-      version='0.1',
+      version='0.1.0.0',
       packages=['evosorocore'],
       description='Evolution toolkit for soft robots',
       install_requires=['numpy', 'scipy', 'networkx', 'lxml']
